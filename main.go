165f4a4bfd891bc361ac44d36c650500036ffadb
WenzheLiu <liuwenzhe2008@qq.com>
2018-02-28 11:24:39 +0800

    get job and run, write state and merged stdout and stderr

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	//	"io/ioutil"
 	"log"
 	//"net/http"
 	//"strings"
@@ -72,6 +72,19 @@ func (worker *Worker) watch(key string, opts ...clientv3.OpOption) clientv3.Watc
 	return worker.cli.Watch(context.Background(), key, opts...)
 }
 
+func (worker *Worker) delete(key string, opts ...clientv3.OpOption) (*clientv3.DeleteResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	defer cancel()
+	return worker.cli.Delete(ctx, key, opts...)
+}
+
+// test:
+// etcdctl put worker/wenzhe
+// etcdctl put job/py10 '["python", "-c", "import time; import sys; print 123; time.sleep(10); print 456; sys.exit(0)"]'
+// etcdctl put job/1 '["gotest"]'
+// etcdctl put assign/wenzhe/py10 ''
+// etcdctl put assign/wenzhe/1 ''
+
 func main() {
 	// get argument
 	flag.Parse()
@@ -137,80 +150,73 @@ func main() {
 	for wresp := range rch {
 		for _, ev := range wresp.Events {
 			switch ev.Type.String() {
-			case "PUTtt":
+			case "PUT":
 				// get the job id
-				jobId, err := strconv.Atoi(string(ev.Kv.Key)[lenAssignNodeKey:])
+				assignJobKey := string(ev.Kv.Key)
+				jobId := assignJobKey[lenAssignNodeKey:]
+				fmt.Println("Run job", jobId)
+				// get the job command by job Id
+				jobIdNodeKey := "job/" + jobId
+				resp, err := worker.get(jobIdNodeKey)
 				if err != nil {
-					fmt.Println("Not a valid jobId in", ev.Kv.Key)
+					fmt.Println(err)
 					continue
 				}
-				fmt.Println("Run job", jobId)
-				//				var jobIds []int
-				//				err = json.Unmarshal(ev.Kv.Value, &jobIds)
-				//				if err != nil {
-				//					fmt.Println(err)
-				//					continue
-				//				}
-				//				// run jobs
-				//				fmt.Printf("Run jobs %v\n", jobIds)
-				//				for _, jobId := range jobIds {
-				if _, ok := worker.jobs[jobId]; !ok {
-					// get the job command by jobId
-					jobIdNodeKey := "job/" + strconv.Itoa(jobId)
-					resp, err := worker.get(jobIdNodeKey)
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					if len(resp.Kvs) == 0 {
-						fmt.Println("No job with id", jobId)
-						continue
-					}
-					var cmdWithArgs []string
-					err = json.Unmarshal(resp.Kvs[0].Value, &cmdWithArgs)
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					if len(cmdWithArgs) == 0 {
-						fmt.Println("No command in the job", jobId)
-						continue
-					}
-					command := cmdWithArgs[0]
-					args := cmdWithArgs[1:]
-					// add to job
-					job := Job{jobId, command, args, JOB_STATUS_PENDING}
-					worker.jobs[jobId] = &job
-					// check resource to run job (parellel count)
-
-					// run job: execute command
-					cmd := exec.Command(command, args...)
-					stdout, err := cmd.StdoutPipe()
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					defer stdout.Close()
-					fmt.Print("Run command", command)
-					for _, arg := range args {
-						fmt.Print(" ", arg)
-					}
-					fmt.Println()
-					if err := cmd.Start(); err != nil {
-						fmt.Println(err)
-						continue
-					}
-					opBytes, err := ioutil.ReadAll(stdout)
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					fmt.Println(string(opBytes))
+				if len(resp.Kvs) == 0 {
+					fmt.Println("No job with id", jobId)
+					continue
 				}
+				var cmdWithArgs []string
+				err = json.Unmarshal(resp.Kvs[0].Value, &cmdWithArgs)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				if len(cmdWithArgs) == 0 {
+					fmt.Println("No command in the job", jobId)
+					continue
+				}
+				command := cmdWithArgs[0]
+				args := cmdWithArgs[1:]
+				// add to job
+				//					job := Job{jobId, command, args, JOB_STATUS_PENDING}
+				//					worker.jobs[jobId] = &job
 
-				//			case "DELETE":
-				//				fmt.Println("Shutdown worker", name)
-				//				os.Exit(0)
+				// check resource to run job (parellel count)
+
+				// set job state: running
+				jobStateFromWorker := jobIdNodeKey + "/state/" + worker.name
+				worker.put(jobStateFromWorker, JOB_STATUS_RUNNING)
+
+				// run job: execute command
+				cmd := exec.Command(command, args...)
+				fmt.Print("Run command: ", command)
+				for _, arg := range args {
+					fmt.Print(" ", arg)
+				}
+				fmt.Println()
+
+				//TODO: use cmd.Start() to async execute, and periodly update the stdout and stderr to node
+				// job/<jobid>/state/<worker>/stdout and stderr, so that user can known its running status.
+				byteBuf, err := cmd.CombinedOutput()
+				jobOutput := string(byteBuf)
+				log.Println(jobOutput)
+
+				// set job output
+				worker.put(jobStateFromWorker+"/stdouterr", jobOutput)
+
+				if err != nil {
+					// if error (exit value is not 0), set job state fail
+					log.Println(err)
+					worker.put(jobStateFromWorker, JOB_STATUS_FAIL)
+				} else {
+					// set job state done
+					worker.put(jobStateFromWorker, JOB_STATUS_DONE)
+				}
+				// remove the assign job assign to the worker, as it is not running or pending by the worker
+				worker.delete(assignJobKey)
+			case "DELETE":
+				// do nothing
 			default:
 				fmt.Printf("Unknown event type %s (%q : %q)\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
 			}
